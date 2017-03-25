b5d66d6e5d0e5378fbf2af980cf01d5c34870db8
mwas <francismwangi152@gmail.com>
2017-03-25 12:22:16 +0300

    removed dead code

--- ussd/utilities.py
+++ ussd/utilities.py
@@ -13,37 +13,3 @@ def str_to_class(import_path):
     except ImportError:
         raise Exception('Module does not exist')
     return class_
-
-
-def get_variables_from_response_obj(response):
-    response_varialbes = {}
-
-    for i in inspect.getmembers(response):
-        # Ignores anything starting with underscore
-        # (that is, private and protected attributes)
-        if not i[0].startswith('_'):
-            # Ignores methods
-            if not inspect.ismethod(i[1]) and \
-                            type(i[1]) in \
-                            (str, dict, int, dict, float, list, tuple):
-                if len(i) == 2:
-                    response_varialbes.update(
-                        {i[0]: i[1]}
-                    )
-
-    try:
-        response_content = json.loads(response.content.decode())
-    except json.JSONDecodeError:
-        response_content = response.content.decode()
-
-    if isinstance(response_content, dict):
-        response_varialbes.update(
-            response_content
-        )
-
-    # update content to save the one that has been decoded
-    response_varialbes.update(
-        {"content": response_content}
-    )
-
-    return response_varialbes
